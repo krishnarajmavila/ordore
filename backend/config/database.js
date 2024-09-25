bd55288441a92f00c8dbc05a9e6cef370c717868
mozkrishna <krishna@mozonto.com>
2024-09-25 16:50:51 +0530

    db deployed

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/ordore', {
+    await mongoose.connect('mongodb+srv://krishnarajmavila:oTxhfAchO2rErhS3@ordorecone.equ25.mongodb.net/', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
